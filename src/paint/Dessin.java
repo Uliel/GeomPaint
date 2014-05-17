d96ca344ab510fafc9eb24f74cd4bfd037c52f9c
Sylvain Riess <sylvain.riess@gmail.com>
2014-05-17 20:14:38 +0200

    Reecriture du dessin du cercle et ajout d'un switch de selection des figures

--- src/paint/Dessin.java
+++ src/paint/Dessin.java
@@ -14,6 +14,8 @@ import java.awt.event.MouseMotionListener;
 import javax.swing.*;
 
 import figures.*;
+// Rq : laisser l'import de figures.Rectangle qui permet a eclipse de ne pas confondre avec une classe java existante java.awt.rectangle
+import figures.Rectangle;
 
 public class Dessin extends JPanel {
 
@@ -40,15 +42,44 @@ public class Dessin extends JPanel {
 				g.setColor(Color.BLACK);
 				//On ne dessine que si un bouton dessin est appuy�
 				if (boutons.getDessiner()) {
-					//Si on clique pour la premi�re fois, on cr�e la figure et on d�selectionne la pr�c�dente
+					//Si on clique pour la premiere fois :
 					if (nbClics == 0) {
-						tabFigures[nbFigures] = new Cercle();
+						// - Creation de la figure selectionnee dans le menu
+						switch(boutons.getNumFigCourante()) {
+						case(1) :
+							tabFigures[nbFigures] = new Cercle();
+							break;
+						case(2) :
+							tabFigures[nbFigures] = new Rectangle();
+							break;
+						case(3) :
+							tabFigures[nbFigures] = new Carre();
+							break;
+						case(4) :
+							tabFigures[nbFigures] = new Triangle();
+							break;
+						case(5) :
+							tabFigures[nbFigures] = new Ovale();
+							break;
+						case(6) :
+							tabFigures[nbFigures] = new Polygone();
+							break;
+						case(7) :
+							tabFigures[nbFigures] = new Losange();
+							break;	
+//						case(8) :
+//							tabFigures[nbFigures] = new Trait();
+//							break;
+						}
 						tabFigures[nbFigures].setCouleur(g.getColor());
+						
+						// - Deselection de la figure precedente
 						for (int i = 0; i < nbFigures; i++) {
 							if (tabFigures[i].getSelection())
 								tabFigures[i].selectionner();
 						}
 					}
+					
 					//On remplit le tableau de points et on incr�mente le nombre de clics
 					if (nbClics < tabFigures[nbFigures].getTabSaisie().length - 1) {
 						tabFigures[nbFigures].ajouterSaisie(nbClics, e.getX(),
@@ -119,23 +150,33 @@ public class Dessin extends JPanel {
 			for (int i = 0; i < nbFigures; i++) {
 				g.setColor(tabFigures[i].getCouleur());
 				Point[] positions = tabFigures[i].getTabSaisie();
-				// Dessin d'un cercle
-				// Calcul de la distance � faire ailleurs
-				int diametre = ((int) Math
-						.sqrt((positions[0].x - positions[1].x)
-								* (positions[0].x - positions[1].x)
-								+ (positions[0].y - positions[1].y)
-								* (positions[0].y - positions[1].y))) * 2;
-				g.drawOval(positions[0].x - diametre / 2, positions[0].y
-						- diametre / 2, diametre, diametre);
-				//Si la figure est s�lectionn�e, on dessine les points de s�lection
-				if (tabFigures[i].getSelection()) {
-					g.drawRect(positions[0].x - 3, positions[0].y - 3, 6, 6);
-					g.drawRect(positions[0].x + diametre / 2 - 3,
-							positions[0].y - 3, 6, 6);
+				
+				// Cas du cercle
+				if(tabFigures[i] instanceof Cercle) {
+					// Recuperation des coordonnees
+					int x0 = positions[0].x;
+					int y0 = positions[0].y;
+					int x1 = positions[1].x;
+					int y1 = positions[1].y;
+					
+					// Calcul du rayon et dessin
+					int rayon = distance(x0, y0, x1, y1);
+					g.drawOval(x0 - rayon, y0 - rayon, rayon * 2, rayon * 2);
+					
+					//Si la figure est selectionnee, on dessine les points de selection (et pas les points de memorisation attention...)
+					if (tabFigures[i].getSelection()) {
+						g.drawRect(x0 - 3, y0 - 3, 6, 6);
+						g.drawRect(x1 - 3, y1 - 3, 6, 6);
+					}
 				}
 			}
 		}
+	}
+	
+	
+	// Methode de calcul de la distance entre 2 points
+	public int distance(int x1, int y1, int x2, int y2) {
+		return (int)Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
 	}
 
 }
