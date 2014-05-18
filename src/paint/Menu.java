da985a259ae3c1ad07d38fd08365b73732f10c41
Nicolas Gambarini <NGamba54@yahoo.fr>
2014-05-18 13:46:59 +0200

    Selection des boutons (couleur bleu) lorsque l'on clique dessus

--- src/paint/Menu.java
+++ src/paint/Menu.java
@@ -1,4 +1,5 @@
 package paint;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -9,60 +10,61 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-
 public class Menu extends JPanel {
-	
+
 	// ATTRIBUTS
 	private boolean dessiner;
-	private JToolBar formes=new JToolBar("Formes");
+	private JToolBar formes = new JToolBar("Formes");
 	private String numFigCourante;
 
-	
 	// CONSTRUCTEUR
 	/**
 	 * Constructeur initialisant le menu
 	 */
 	public Menu() {
-		// Creation des boutons de selection de forme et stockage dans un tableau
-		formes.setLayout(new GridLayout(2,4,2,2));
+		// Creation des boutons de selection de forme et stockage dans un
+		// tableau
+		formes.setLayout(new GridLayout(2, 4, 2, 2));
 		Bouton cercle = new Bouton("1", new ImageIcon("images/cercle.jpg"));
-		Bouton rectangle = new Bouton("2", new ImageIcon("images/rectangle.jpg"));
+		Bouton rectangle = new Bouton("2",
+				new ImageIcon("images/rectangle.jpg"));
 		Bouton carre = new Bouton("3", new ImageIcon("images/carre.jpg"));
 		Bouton triangle = new Bouton("4", new ImageIcon("images/triangle.jpg"));
 		Bouton ellipse = new Bouton("5", new ImageIcon("images/ovale.jpg"));
 		Bouton polygone = new Bouton("6", new ImageIcon("images/polygone.jpg"));
 		Bouton losange = new Bouton("7", new ImageIcon("images/losange.jpg"));
 		Bouton trait = new Bouton("8", new ImageIcon("images/trait.jpg"));
-		Bouton[] tabBoutonsFormes = {cercle, rectangle, carre, triangle, ellipse, polygone, losange, trait};
-		
+		final Bouton[] tabBoutonsFormes = { cercle, rectangle, carre, triangle,
+				ellipse, polygone, losange, trait };
 		// Creation d'un auditeur commun a tous les boutons
 		ActionListener selectionForme = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dessiner = true;
-				numFigCourante = ((Bouton)(e.getSource())).getValeur();
+				for (int i = 0; i < tabBoutonsFormes.length; i++) {
+					tabBoutonsFormes[i].setSelected(false);
+				}
+				numFigCourante = ((Bouton) (e.getSource())).getValeur();
+				((Bouton) (e.getSource())).setSelected(true);
 			}
 		};
-		
-		/* Pour tous les boutons :
-		 *  - Affectation de l'auditeur
-		 *  - Coloration du background
-		 *  - Attachement à la ToolBar
+		/*
+		 * Pour tous les boutons : - Affectation de l'auditeur - Coloration du
+		 * background - Attachement à la ToolBar
 		 */
-		for (int i = 0 ; i < tabBoutonsFormes.length ; i++) {
+		for (int i = 0; i < tabBoutonsFormes.length; i++) {
 			tabBoutonsFormes[i].setBackground(Color.white);
 			tabBoutonsFormes[i].addActionListener(selectionForme);
 			formes.add(tabBoutonsFormes[i]);
 		}
-		
+
 		this.add(formes);
 	}
-	
-	
+
 	// ACCESSEURS
 	public boolean getDessiner() {
 		return this.dessiner;
 	}
-	
+
 	public void setDessiner(boolean d) {
 		this.dessiner = d;
 	}
