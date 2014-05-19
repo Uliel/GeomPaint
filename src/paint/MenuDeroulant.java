1aa5bb9cd76d985144da9680e27acc2ac0aec35c
Nicolas Gambarini <NGamba54@yahoo.fr>
2014-05-19 22:30:44 +0200

    amelioration menu deroulant (ajout des listeners)

--- src/paint/MenuDeroulant.java
+++ src/paint/MenuDeroulant.java
@@ -4,15 +4,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JMenuItem;
-import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.border.BevelBorder;
-import javax.swing.event.PopupMenuEvent;
-import javax.swing.event.PopupMenuListener;
 
 public class MenuDeroulant extends JPopupMenu{
 Dessin dessin;
@@ -20,6 +15,21 @@ Dessin dessin;
 	  dessin=d;
     ActionListener menuListener = new ActionListener() {
       public void actionPerformed(ActionEvent event) {
+    	  if (event.getActionCommand().equals("Quitter")) {
+    		  System.exit(0);
+    	  }
+    	  else if (event.getActionCommand().equals("Exporter en jpg")) {
+    		  dessin.exporter("jpg");
+    	  }
+    	  else if (event.getActionCommand().equals("Exporter en png")) {
+    		  dessin.exporter("png");
+    	  }
+    	  else if (event.getActionCommand().equals("Supprimer")) {
+    		  dessin.supprimer();
+    	  }
+
+
+    	  
         //System.out.println("Popup menu item ["
             //+ event.getActionCommand() + "] was pressed.");
       }
@@ -41,27 +51,7 @@ Dessin dessin;
     this.add(item = new JMenuItem("Quitter"));
     item.addActionListener(menuListener);
 
-    this.setLabel("Justification");
     this.setBorder(new BevelBorder(BevelBorder.RAISED));
-
-    addMouseListener(new MousePopupListener());
   }
 
-  // An inner class to check whether mouse events are the popup trigger
-  class MousePopupListener extends MouseAdapter {
-    public void mousePressed(MouseEvent e) {
-      checkPopup(e);
-    }
-
-    public void mouseClicked(MouseEvent e) {
-      checkPopup(e);
-    }
-
-    public void mouseReleased(MouseEvent e) {
-      checkPopup(e);
-    }
-
-    private void checkPopup(MouseEvent e) {
-    }
-  }
 }
