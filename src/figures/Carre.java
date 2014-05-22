d5d2cc86a9029ce5bd7be450ad1e260c9a64b0d1
Sylvain Riess <sylvain.riess@gmail.com>
2014-05-22 21:46:19 +0200

    simplification de la classe Carre

--- src/figures/Carre.java
+++ src/figures/Carre.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class Carre extends Rectangle {
 
-	// CONSTRUCTEURS
+	// CONSTRUCTEUR
 
 	/**
 	 * Constructeur prenant en parametre une ArrayList (2 Pts) - le premier
@@ -28,50 +28,21 @@ public class Carre extends Rectangle {
 
 		// calcul de la longueur des cotes du carre
 		int d = listePointsSaisie.get(0).x - listePointsSaisie.get(1).x;
-		if (d < 0) {
+		if (d < 0)
 			d = -d;
-		}
 
-		// 1er point en haut a gauche, 2nd en bas a droite
-		if (listePointsSaisie.get(1).x > listePointsSaisie.get(0).x) {
-			if (listePointsSaisie.get(1).y > listePointsSaisie.get(0).y) {
-				this.tabMemo[0] = listePointsSaisie.get(0);
-				this.tabMemo[1] = new UnPoint(listePointsSaisie.get(0).x + d,
-						listePointsSaisie.get(0).y);
-				this.tabMemo[2] = new UnPoint(listePointsSaisie.get(0).x + d,
-						listePointsSaisie.get(0).y + d);
-				this.tabMemo[3] = new UnPoint(listePointsSaisie.get(0).x,
-						listePointsSaisie.get(0).y + d);
-			} // 1er point en bas a gauche, 2nd en haut a droite
-			else {
-				this.tabMemo[0] = new UnPoint(listePointsSaisie.get(0).x,
-						listePointsSaisie.get(0).y - d);
-				this.tabMemo[1] = new UnPoint(listePointsSaisie.get(0).x + d,
-						listePointsSaisie.get(0).y - d);
-				this.tabMemo[2] = new UnPoint(listePointsSaisie.get(0).x + d,
-						listePointsSaisie.get(0).y);
-				this.tabMemo[3] = listePointsSaisie.get(0);
-			}
-		} // 1er point en haut a droite, 2nd en bas a gauche
-		else {
-			if (listePointsSaisie.get(1).y > listePointsSaisie.get(0).y) {
-				this.tabMemo[0] = new UnPoint(listePointsSaisie.get(0).x - d, listePointsSaisie.get(0).y);
-				this.tabMemo[1] = listePointsSaisie.get(0);
-				this.tabMemo[2] = new UnPoint(listePointsSaisie.get(0).x, listePointsSaisie.get(0).y + d);
-				this.tabMemo[3] = new UnPoint(tabMemo[0].x, tabMemo[2].y);
-			} // 1er point en bas a droite, 2nd en haut a gauche
-			else {
-				this.tabMemo[0] = new UnPoint(listePointsSaisie.get(0).x - d,
-						listePointsSaisie.get(0).y - d);
-				this.tabMemo[1] = new UnPoint(listePointsSaisie.get(0).x,
-						listePointsSaisie.get(0).y - d);
-				this.tabMemo[2] = listePointsSaisie.get(0);
-				this.tabMemo[3] = new UnPoint(listePointsSaisie.get(0).x - d,
-						listePointsSaisie.get(0).y);
-			}
-		}
+		this.tabMemo[0] = listePointsSaisie.get(0);
+		this.tabMemo[1] = new UnPoint(listePointsSaisie.get(0).x + d,
+				listePointsSaisie.get(0).y);
+		this.tabMemo[2] = new UnPoint(listePointsSaisie.get(0).x + d,
+				listePointsSaisie.get(0).y + d);
+		this.tabMemo[3] = new UnPoint(listePointsSaisie.get(0).x,
+				listePointsSaisie.get(0).y + d);
 	}
 
+	
+	// METHODE
+	
 	/**
 	 * Redimensionnement specifique au carre
 	 * 
