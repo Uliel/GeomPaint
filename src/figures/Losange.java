ef6698398883c5af575c57b405af69f42289157d
fredericeuriot <frederic.euriot@yahoo.fr>
2014-05-22 16:13:47 +0200

    commit qui sert a rien (ou presque)

--- src/figures/Losange.java
+++ src/figures/Losange.java
@@ -46,4 +46,44 @@ public class Losange extends Polygone {
 																	listePointsSaisie.get(0).y-hauteur);
 			
 	}
+	
+	/**
+	 * Deplacement specifique au rectangle
+	 * 
+	 * @param pt
+	 *            le point modifie
+	 * @param valAbs
+	 *            la nouvelle valeur de l'abscisse
+	 * @param valOrd
+	 *            la nouvelle valeur de l'ordonnee
+	 */
+	public void modifierTaille(UnPoint pt, int valAbs, int valOrd) {
+		
+	//calcul du centre du losange
+			UnPoint centre = new UnPoint(tabMemo[1].x + tabMemo[1].x - tabMemo[3].x,
+																	 tabMemo[0].y + tabMemo[0].y - tabMemo[2].y);
+		
+		if (pt.equals(tabMemo[0])) {
+			this.tabMemo[0].move(tabMemo[0].x + valAbs, tabMemo[0].y + valOrd);
+			this.tabMemo[1].move(tabMemo[1].x, tabMemo[1].y + valOrd);
+			this.tabMemo[2].move(tabMemo[2].x, tabMemo[2].y);
+			this.tabMemo[3].move(tabMemo[3].x + valAbs, tabMemo[3].y);
+		} else if (pt.equals(tabMemo[1])) {
+			this.tabMemo[0].move(tabMemo[0].x, tabMemo[0].y + valOrd);
+			this.tabMemo[1].move(tabMemo[1].x + valAbs, tabMemo[1].y + valOrd);
+			this.tabMemo[2].move(tabMemo[2].x + valAbs, tabMemo[2].y);
+			this.tabMemo[3].move(tabMemo[3].x, tabMemo[3].y);
+		} else if (pt.equals(tabMemo[2])) {
+			this.tabMemo[0].move(tabMemo[0].x, tabMemo[0].y);
+			this.tabMemo[1].move(tabMemo[1].x + valAbs, tabMemo[1].y);
+			this.tabMemo[2].move(tabMemo[2].x + valAbs, tabMemo[2].y + valOrd);
+			this.tabMemo[3].move(tabMemo[3].x, tabMemo[3].y + valOrd);
+		} else if (pt.equals(tabMemo[3])) {
+			this.tabMemo[0].move(tabMemo[0].x + valAbs, tabMemo[0].y);
+			this.tabMemo[1].move(tabMemo[1].x, tabMemo[1].y);
+			this.tabMemo[2].move(tabMemo[2].x, tabMemo[2].y + valOrd);
+			this.tabMemo[3].move(tabMemo[3].x + valAbs, tabMemo[3].y + valOrd);
+		}
+	}
+
 }
